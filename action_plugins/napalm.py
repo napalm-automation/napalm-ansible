871ac8ceb652e371951a29274916ec3f538e2534
targuan <targuan@gmail.com>
2017-07-16 12:45:06 +0000

    Add default connection parameters

--- action_plugins/napalm.py
+++ action_plugins/napalm.py
@@ -6,21 +6,25 @@ import re
 import time
 import glob
 
+import ansible
 from ansible.plugins.action.normal import ActionModule as _ActionModule
-from pprint import pprint
+
+from distutils.version import LooseVersion
 
 class ActionModule(_ActionModule):
     def run(self, tmp=None, task_vars=None):
         pc = self._play_context
 
-        provider = self._task.args.get('provider', {})
+        if hasattr(pc, "connection_user"): # new in ansible 2.3
+            # populate provider values with context values if not set
+            provider = self._task.args.get('provider', {})
 
-        provider['hostname'] = provider.get('hostname', provider.get('host', pc.remote_addr))
-        provider['username'] = provider.get('username', pc.connection_user)
-        provider['password'] = provider.get('password', pc.password)
-        provider['timeout'] = provider.get('timeout', pc.timeout)
+            provider['hostname'] = provider.get('hostname', provider.get('host', pc.remote_addr))
+            provider['username'] = provider.get('username', pc.connection_user)
+            provider['password'] = provider.get('password', pc.password)
+            provider['timeout'] = provider.get('timeout', pc.timeout)
 
-        self._task.args['provider'] = provider
+            self._task.args['provider'] = provider
 
         result = super(ActionModule, self).run(tmp, task_vars)
         return result
