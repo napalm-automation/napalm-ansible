3409b38d49fc436f6afe332bc0a9785eb655aa96
David Barroso <dbarrosop@dravetech.com>
2017-08-02 20:47:22 +0200

    making pylama happy

--- napalm_ansible/napalm_cli.py
+++ napalm_ansible/napalm_cli.py
@@ -59,7 +59,7 @@ else:
 
 
 def main():
-    os_choices = ['eos', 'junos', 'iosxr', 'fortios', 'ibm',
+    os_choices = ['eos', 'junos', 'iosxr', 'fortios',
                   'ios', 'mock', 'nxos', 'panos', 'vyos', 'ros']
     module = AnsibleModule(
         argument_spec=dict(
