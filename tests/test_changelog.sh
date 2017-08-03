ec855ddb6461506cbdf0b324e191142a3b55c3fc
David Barroso <dbarrosop@dravetech.com>
2017-08-03 15:48:30 +0200

    Only do changelog check if it's a PR not coming from develop

--- tests/test_changelog.sh
+++ tests/test_changelog.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+echo TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+echo TRAVIS_TAG=$TRAVIS_TAG
 if [[ ( ! -z $TRAVIS_PULL_REQUEST_BRANCH ) && ( $TRAVIS_PULL_REQUEST_BRANCH != "develop" ) && ( -z $TRAVIS_TAG ) ]]; then
-	[ ! -z "$(git diff develop... -- ../CHANGELOG.rst)" ] || ( echo "Remember to update the CHANGELOG.rst" && return 1 )
+	echo "Checking for CHANGELOG changes"
+	[ ! -z "$(git diff develop... -- ../CHANGELOG.rst)" ] || ( echo "Remember to update the CHANGELOG.rst" && exit 1 )
+else
+	echo "Not checking for CHANGELOG changes"
 fi
