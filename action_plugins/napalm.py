8dc2ecaeb1f6aa4861b1bba9b42d4285b8f13320
targuan <targuan@gmail.com>
2017-08-07 22:06:41 +0000

    Remova unused import

--- action_plugins/napalm.py
+++ action_plugins/napalm.py
@@ -1,22 +1,15 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import os
-import re
-import time
-import glob
-
-import ansible
 from ansible.plugins.action.normal import ActionModule as _ActionModule
 
-from distutils.version import LooseVersion
 
 class ActionModule(_ActionModule):
     def run(self, tmp=None, task_vars=None):
         pc = self._play_context
 
         if hasattr(pc, "connection_user"): # new in ansible 2.3
-            # populate provider values with context values if not set
+            #  populate provider values with context values if not set
             provider = self._task.args.get('provider', {})
 
             provider['hostname'] = provider.get('hostname', provider.get('host', pc.remote_addr))
