585cae20e04e3b14cc12c499df73db6cdd18f892
targuan <targuan@gmail.com>
2017-08-09 15:16:00 +0000

    Display action_plugins message only for ansible > 2.3

--- napalm_ansible/__init__.py
+++ napalm_ansible/__init__.py
@@ -1,4 +1,6 @@
 import os
+import ansible
+from distutils.version import LooseVersion
 
 message = """
 To make sure ansible can make use of the napalm modules you will have
@@ -7,14 +9,21 @@ file, i.e. `./ansible.cfg`:
 
     [defaults]
     library = {path}
-    action_plugins = {action_path}
+    {action_plugins}
 
 For more details on ansible's configuration file visit:
 https://docs.ansible.com/ansible/latest/intro_configuration.html
 """
 
+message_plugins = "action_plugins = {action_path}"
+
 
 def main():
     path = os.path.dirname(__file__)
-    action_path = os.path.abspath(os.path.join(path, '..', 'action_plugins'))
-    print(message.format(path=path, action_path=action_path).strip())
+    if LooseVersion(ansible.__version__) < LooseVersion('2.3.0.0'):
+        action_plugins = ""
+    else:
+        action_path = os.path.abspath(os.path.join(path, '..', 'action_plugins'))
+        action_plugins = message_plugins.format(action_path=action_path)
+
+    print(message.format(path=path, action_plugins=action_plugins).strip())
