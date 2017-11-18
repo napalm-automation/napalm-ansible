8d453a8f04b2f519484f84c9b852c22facda92b5
Patrick Ogenstad <patrick@ogenstad.com>
2017-11-18 08:16:16 +0100

    Generate json files with documentation for modules
    Content for the RETURN variable will be added later.

--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pytest
 import yaml
 from glob import glob
@@ -27,3 +29,17 @@ def test_module_documentation_format(ansible_module):
 def test_module_examples_format(ansible_module):
     module = import_module(ansible_module)
     yaml.load(module.EXAMPLES)
+
+
+def test_build_docs(ansible_module):
+    try:
+        os.mkdir('module_docs')
+    except:
+        pass
+    module = import_module(ansible_module)
+    content = {}
+    content['doc'] = yaml.load(module.DOCUMENTATION)
+    content['examples'] = yaml.load(module.EXAMPLES)
+    module = ansible_module.replace('napalm_ansible.', '')
+    with open('module_docs/{0}.json'.format(module), 'w') as f:
+        json.dump(content, f, indent=4, sort_keys=True)
