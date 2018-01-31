bc34f79276485921b68d6977c7a783d9cd76815d
Kirk Byers <ktbyers@twb-tech.com>
2018-01-31 10:15:08 -0800

    Fixing action plugin reference in message string

--- napalm_ansible/__init__.py
+++ napalm_ansible/__init__.py
@@ -3,9 +3,9 @@ import ansible
 from distutils.version import LooseVersion
 
 message = """
-To make sure ansible can make use of the napalm modules you will have
-to add the following configurtion to your ansible configureation
-file, i.e. `./ansible.cfg`:
+To ensure Ansible can use the NAPALM modules you will have
+to add the following configurtion to your Ansible configuration
+file (ansible.cfg):
 
     [defaults]
     library = {path}/modules
@@ -15,15 +15,12 @@ For more details on ansible's configuration file visit:
 https://docs.ansible.com/ansible/latest/intro_configuration.html
 """
 
-message_plugins = "action_plugins = {action_path}"
-
 
 def main():
     path = os.path.dirname(__file__)
     if LooseVersion(ansible.__version__) < LooseVersion('2.3.0.0'):
         action_plugins = ""
     else:
-        action_path = os.path.abspath(os.path.join(path, '..', 'plugins/action'))
-        action_plugins = message_plugins.format(action_path=action_path)
+        action_plugins = "action_plugins = {path}/plugins/action".format(path=path)
 
     print(message.format(path=path, action_plugins=action_plugins).strip())
