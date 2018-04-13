0823d396e850a54402281dd376f4d1afa8b4aad9
Kirk Byers <ktbyers@twb-tech.com>
2018-04-13 02:04:28 -0700

    Fixing PIP10 compatibility issue (#125)

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 """setup.py file."""
-import uuid
-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
-
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
 
 setup(
     name="napalm-ansible",
