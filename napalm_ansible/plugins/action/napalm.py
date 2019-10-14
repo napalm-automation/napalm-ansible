75231a5df48190672fb03e012f074991f186faf0
Kirk Byers <ktbyers@twb-tech.com>
2019-10-14 12:33:02 -0700

    Fixing -u user issue

--- napalm_ansible/plugins/action/napalm.py
+++ napalm_ansible/plugins/action/napalm.py
@@ -8,12 +8,12 @@ class ActionModule(_ActionModule):
     def run(self, tmp=None, task_vars=None):
         pc = self._play_context
 
-        if hasattr(pc, "connection_user"):  # new in ansible 2.3
-            # populate provider values with context values if not set
+        if hasattr(pc, "remote_user"):
+            # Populate provider values with context values if not set
             provider = self._task.args.get('provider', {})
 
             provider['hostname'] = provider.get('hostname', provider.get('host', pc.remote_addr))
-            provider['username'] = provider.get('username', pc.connection_user)
+            provider['username'] = provider.get('username', pc.remote_user)
             provider['password'] = provider.get('password', pc.password)
             # Timeout can't be passed via command-line as Ansible defaults to a 10 second timeout
             provider['timeout'] = provider.get('timeout', 60)
